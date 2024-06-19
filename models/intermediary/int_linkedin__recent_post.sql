014ed0c551dc63fe564aa5f0ad10e58ccd76835e
Edward Hayter <egkhayter@gmail.com>
2024-06-19 14:45:49 +0000

    created intermediary models and two mart tables. One for head office that breaks down daily performance by org and one for the marketing team that contains every post made across sites along with post details

---
+++ models/intermediary/int_linkedin__recent_post.sql
@@ -0,0 +1,28 @@
+-- import ctes
+with 
+
+posts as (
+
+    select * from {{ ref('stg_linkedin__post_history') }}
+    where author_type = 'organization'
+
+),
+
+final as (
+
+    select 
+        
+        posts.post_id,
+        max(posts.post_modified_time) as recent_edit,
+        count(posts.post_modified_time) as num_edits,
+        posts.post_created_date,
+        posts.initial_publish_date
+    
+    from posts
+
+    group by posts.post_id, posts.post_created_date, posts.initial_publish_date
+
+)
+
+select * from final
+
--- models/staging/stg_linkedin__post_content.sql
+++ models/staging/stg_linkedin__post_content.sql
@@ -12,9 +12,9 @@ transformed as (
         replace(post_id, 'urn:li:ugcPost:', '') as post_id,
         type as post_type,
         case 
-            when contains(media_id, 'document') then 'urn:li:document:'
-            when contains(media_id, 'video') then 'urn:li:video:'
-            when contains(media_id, 'image') then 'urn:li:image:'
+            when contains(media_id, 'document') then 'document:'
+            when contains(media_id, 'video') then 'video:'
+            when contains(media_id, 'image') then 'image:'
             else null
         end as media_type,
         replace(media_id, case 
